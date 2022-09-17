3c02d55a696612eac277d6885790e0882a28594b
Marko <ilya1501@gmail.com>
2022-09-17 20:29:08 +0300

    DescriptionOfProperties

---
+++ Students/Students.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Students
+{
+   public class Students
+    {
+        public string NameStudents { get; set; }
+        public int AgeStudents { get; set; }
+        public int AcademicPerformance { get; set; }
+        public int Sex { get; set; }
+        public bool BoolIsExpelled { get; set; }
+
+        public Students(string name, int age, int sex, int AP, bool BE)
+        {
+            NameStudents = name;
+            AgeStudents = age;
+            Sex = sex;
+            AcademicPerformance = AP;
+            BoolIsExpelled = BE;
+
+
+        }
+    }
+}
