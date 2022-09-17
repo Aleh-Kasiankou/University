17f15c08d7b4ab6986015a54ecca6b11471ca757
Thel2022 <r.r.pendrick@gmail.com>
2022-09-18 00:51:39 +0200

    Add fields and methods in class Teacher

--- Teacher.cs
+++ Teacher.cs
@@ -1,7 +1,28 @@
-﻿namespace University
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace University
 {
-    public class Teacher
+    public class Teacher : Person
     {
-        
+        public int Teachinglevel { get; set; }
+        public string Subject { get; set; }
+
+        public Teacher(string name, int sex, string subject) : base(name, sex)
+        {
+            Random x = new Random();
+            int teachinglevel = x.Next(0, 10);
+            Teachinglevel = teachinglevel;
+            Subject = subject;
+        }
+        public int TL1()
+        {
+            return Teachinglevel;
+        }
+        public void ConductingAnExam()
+        {
+
+        }
+
     }
-}
+    }
