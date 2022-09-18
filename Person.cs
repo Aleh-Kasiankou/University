0961c43e065c35641201fc93b90192c92d74a206
Thel2022 <r.r.pendrick@gmail.com>
2022-09-18 13:09:50 +0200

    add int sex

--- Person.cs
+++ Person.cs
@@ -8,10 +8,11 @@ namespace University
         public Sex Sex { get; }
         
 
-        public Person(string name)
+        public Person(string name, int sex)
         {
             Name = name;
             Sex = (Sex) new Random().Next(0,2);
+            sex = (int)Convert.ToUInt32(Sex);
         }
     }
     
