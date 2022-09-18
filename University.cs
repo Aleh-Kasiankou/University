75d9968f0485db644c10cf6453b3f682cb23e787
Aleh Kasiankou <aleh.kasiankou@gmail.com>
2022-09-18 14:03:13 +0300

    Implemented Subject class
    Added DataProvider.cs class for Names and Sex generation
    Hid Teacher constructor to automatically assign teacher to a Subject instance
    Added example for student creation in Program.cs

--- University.cs
+++ University.cs
@@ -7,14 +7,15 @@ namespace University
         public string Name { get; set; }
         public List<Teacher> Staff { get; set; } = new List<Teacher>();
         public List<Student> Student { get; set; } = new List<Student>();
-        public List<Subject> Subject { get; set; } = new List<Subject>();
+        public List<Subject> Subjects { get; set; } = new List<Subject>();
 
         public University(string name)
         {
             Name = name;
         }
 
-        public void HireStaff()
+        public void HireStaff() //here we will create both subjects and teachers (potentially could create a
+                                                                                    //new method for subjects)
         {
         }
         
