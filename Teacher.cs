153ec086d0d77baeba1b4db778c6b1dd1c67c793
Aleh Kasiankou <aleh.kasiankou@gmail.com>
2022-09-18 11:09:56 +0300

    Suggested changes to Teacher class

--- Teacher.cs
+++ Teacher.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+
 namespace University
 {
     public class Teacher : Person
     {
-        public int Teachinglevel { get; set; }
-        public string Subject { get; set; }
+        public int TeachingLevel { get; set; }
+        public Subject Subject { get; set; } //switched type to class subject that will be used in the program
 
-        public Teacher(string name, int sex, string subject) : base(name, sex)
+        public Teacher(string name, int sex, Subject subject) : base(name, sex)     // Will cause merge conflicts.
+                                                                                    // Need to resolve them during merge. 
         {
-            Random x = new Random();
-            int teachinglevel = x.Next(0, 10);
-            Teachinglevel = teachinglevel;
+            int randomTeachingLevel = new Random().Next(0, 11); 
+            // the maximal value is not included in range, so changed 10 to 11
+            TeachingLevel = randomTeachingLevel;
             Subject = subject;
         }
-        public int TL1()
-        {
-            return Teachinglevel;
-        }
-        public void ConductingAnExam()
-        {
 
+        public void ConductAnExam() //used infinitive form to denote non-continuous action
+        {
         }
-
     }
-    }
+}
